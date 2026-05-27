37535311220047d6fafe41c801f0ce113c76fe62
cdman523 <codingman0523@gmail.com>
2026-05-27 19:58:49 +0900

    1
    1

---
+++ behaves.py
@@ -0,0 +1,32 @@
+from abc import *
+import random as r
+import math as m
+from main import Animal,World,Vector
+    
+class Behaves(ABC):
+    @abstractmethod
+    def act(self, entity, world):
+        pass
+
+#예시 코드
+#예시1-JUMP는 speed가 10 이상, 배고픔이 10 이상이여야 할 수 있고, 실행시 pos를 +val,+val한다
+class EXAMPLE_JUMPING():
+    def __init__(self,val):
+        self.val=val
+    def act(self,entity,world):
+        if entity.speed<10 or entity.hunger<10:
+            return False
+        entity.pos+=Vector(self.val,self.val)
+        return True
+#예시2-WALK는 배고픔이 4 이상이면 할 수 있다
+class EXAMPLE_WALK():
+    def act(self,entity,world):
+        if entity.hunger<4:
+            return False
+        entity.pos+=Vector(4,4)
+        return True
+#예시3-HELLO는 언제나 할 수 있다
+class EXAMPLE_HELLO():
+    def act(self,entity,world):
+        print('hello!!!!!!!!!!!!!')
+        return True
