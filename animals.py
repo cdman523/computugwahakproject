97b1b7075ba4aa9bf885ae9b4c606a935ef9ca24
yamax882 <keumsunglim1@gmail.com>
2026-06-02 09:05:54 +0900

    Update animals.py

--- animals.py
+++ animals.py
@@ -11,4 +11,10 @@ class ALEPHANT_THE_LEGEND_ANIMAL_IS_BY_SUWOO_MOONSUWOO_GU_NEN_GA_HI_SIN_HWA_LA_G
         return [EXAMPLE_JUMPING(self, 10), EXAMPLE_WALK(self), EXAMPLE_HELLO(self)]
 
 class Buffalo(Animal):
+    def habit(self):
+        return [CHARGE_RUSH(self), BUFFALO_MOVE_TO_BUFFALO(self)]
+
+class Gazelle(Animal):
+    def habit(self):
+        return [GAZELLE_MOVE_TO_GAZELLE(self)]
     
